ecb46df45c7b1e9d492abd4cd6e11740ecbda0df
DeograciousAggrey <deograciousaggrey@gmail.com>
2022-10-06 10:44:22 +0300

    Updated the config files

--- celogames-ui/next.config.js
+++ celogames-ui/next.config.js
@@ -16,3 +16,15 @@ const nextConfig = {
 };
 
 module.exports = withPWA(nextConfig);
+module.exports = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      child_process: false,
+      readline: false,
+    };
+    return config;
+  },
+};
